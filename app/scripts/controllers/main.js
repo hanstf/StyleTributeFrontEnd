69de9c8377e9ebf7cc8199e4495669f4ea3da033
hanstf <hansbrians@gmail.com>
2016-10-22 20:25:47 +0800

    Initial commit based on generator-angular

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name styleTributeFrontEndApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the styleTributeFrontEndApp
+ */
+angular.module('styleTributeFrontEndApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
